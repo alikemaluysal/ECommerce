f4e81fab429d4da5c4b259aef984bb44476289a2
Ali Kemal Uysal <alikemaluysal4235@gmail.com>
2024-03-05 18:52:04 +0300

    feat(WebMVC.Client): add ApiService

---
+++ src/eCommerce/WebMVC.Client/HttpClientHelpers/Exceptions/BaseApiException.cs
@@ -0,0 +1,14 @@
+﻿using WebMVC.Client.HttpClientHelpers.Responses;
+
+namespace WebMVC.Client.HttpClientHelpers.Exceptions;
+
+public class BaseApiException : Exception
+{
+    public ApiErrorResponse ErrorResponse { get; private set; }
+
+    public BaseApiException(ApiErrorResponse errorResponse)
+        : base(errorResponse?.Detail)
+    {
+        ErrorResponse = errorResponse;
+    }
+}
--- src/eCommerce/WebMVC.Client/Program.cs
+++ src/eCommerce/WebMVC.Client/Program.cs
@@ -1,19 +1,23 @@
+using WebMVC.Client.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IApiService, ApiService>();
 
-        var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
+var app = builder.Build();
 
-        app.UseHttpsRedirection();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
